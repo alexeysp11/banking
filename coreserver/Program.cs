391df56577c0c57a9710fd225f59fc6cda70d138
alexeysp11 <alexeyspiridon1111@gmail.com>
2023-04-13 20:44:33 +0500

    configurator for server

--- coreserver/Program.cs
+++ coreserver/Program.cs
@@ -6,9 +6,6 @@ namespace Banking.CoreServer
         {
             System.Console.WriteLine("Start core server"); 
 
-            // string testPath = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\..\\coreserver\\config.json"); 
-            // System.Console.WriteLine("test path: " + testPath); 
-            // string fullpath = @"C:\Users\123\Documents\projects\banking\coreserver\config.json"; 
             string fullpath = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\..\\coreserver\\config.json"); 
             (new Banking.Network.BankingHttpServer(fullpath)).CreateWebServer();
             System.Console.ReadLine();
